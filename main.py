3674e4e0dc6fbb066088699daae10b362d979bfe
moodhossam99-gif <mood.hossam99@gmail.com>
2026-08-20 19:59:55 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,9 @@
+import os
+
+API_KEY = os.getenv("API_KEY")
+PROJECT_ID = os.getenv("PROJECT_ID")
+
+if not API_KEY:
+    raise ValueError("API_KEY is missing from environment variables!")
+
+print("Keys loaded successfully. Starting automation...")
